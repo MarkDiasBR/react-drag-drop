cd43d0f36300b5f2271cfc531f931fbb6b707758
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-05-03 01:49:01 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import {useSpring, animated} from 'react-spring';
+import {useDrag} from '@use-gesture/react';
 
 function App() {
+  const [logoPos, setLogoPos] = useState({ x: 0, y: 0 })
+  const bindLogoPos = useDrag((params)=>{
+    setLogoPos({
+      x: params.offset[0],
+      y: params.offset[1]
+    })
+  });
+
+  const logoPosSpring = useSpring({ x: 0, y: 0 })
+  const bindLogoPosSpring = useDrag((params)=>{
+      logoPosSpring.x.set(params.offset[0]);
+      logoPosSpring.y.set(params.offset[1]);
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <div {...bindLogoPos()} style={{position:'relative', top: logoPos.y, left: logoPos.x}}>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+          
+        <animated.div {...bindLogoPosSpring()} style={{x: logoPosSpring.x, y: logoPosSpring.y}}>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </animated.div>
+
         <a
           className="App-link"
           href="https://reactjs.org"
